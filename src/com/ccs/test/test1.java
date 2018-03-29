3d2756f221811c57c88f3f8ce01cddf1e9989885
NoControl <804937921@qq.com>
2018-03-29 08:42:15 +0800

    更改数据库

--- src/com/ccs/test/test1.java
+++ src/com/ccs/test/test1.java
@@ -1,8 +1,30 @@
+
 package com.ccs.test;
 
 /**
  * Created by NoControl on 2018/3/16.
  */
-public class test1 {
 
+class Vehicle{
+    public void drive(){
+        System.out.println("Vehicle:drive");
+    }
+}
+class Car extends Vehicle{
+    public void drive(){
+        System.out.println("Car:drive");
+    }
+}
+
+public class test1 {
+    public static void main(String args[]){
+        Vehicle v;
+        Car c;
+        v = new Vehicle();
+        c = new Car();
+        v.drive();
+        c.drive();
+        v = c;
+        v.drive();
+    }
 }
